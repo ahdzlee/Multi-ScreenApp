10394889583354768c5ae16315a3f8de12dcbfb0
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-08-02 15:53:58 +0800

    Implemented layouts for first and second screen. Implemented launching of activities.

---
+++ app/src/main/java/tech/hyperdev/multi_screenapp/SecondActivity.java
@@ -0,0 +1,18 @@
+package tech.hyperdev.multi_screenapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void reply(View view) {
+        finish();
+    }
+}
