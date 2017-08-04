6e6dc6a87a9d62946531103c77ab612033070b18
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-08-04 15:33:19 +0800

    Implemented passing of string data to second activity with data coming from EditText.

--- app/src/main/java/tech/hyperdev/multi_screenapp/MainActivity.java
+++ app/src/main/java/tech/hyperdev/multi_screenapp/MainActivity.java
@@ -4,19 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtMessage;
+
+    public static final String KEY_SEND_MESSAGE = "key-sent-message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+    }
+
+    private void findViews() {
+        mEtMessage = (EditText) findViewById(R.id.etMessage);
     }
 
     public void send(View view) {
-        String sendMe = "This is the message I want to send!";
+        String sendMe = mEtMessage.getText().toString();
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("key-sent-message", sendMe);
+        intent.putExtra(KEY_SEND_MESSAGE, sendMe);
         startActivity(intent);
     }
 }
--- app/src/main/java/tech/hyperdev/multi_screenapp/SecondActivity.java
+++ app/src/main/java/tech/hyperdev/multi_screenapp/SecondActivity.java
@@ -3,21 +3,38 @@ package tech.hyperdev.multi_screenapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView mTvSentMsgHolder;
+    private TextView mTvSentMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        findViews();
+
         Intent intent = getIntent();
         if (intent != null) {
-            String sentMessage = intent.getStringExtra("key-sent-message");
-            Toast.makeText(this, sentMessage, Toast.LENGTH_SHORT).show();
+            String sentMessage = intent.getStringExtra(MainActivity.KEY_SEND_MESSAGE);
+            if (!TextUtils.isEmpty(sentMessage)) {
+                mTvSentMsgHolder.setVisibility(View.VISIBLE);
+                mTvSentMsg.setVisibility(View.VISIBLE);
+
+                mTvSentMsg.setText(sentMessage);
+            }
         }
+    }
+
+    private void findViews() {
+        mTvSentMsgHolder = (TextView) findViewById(R.id.tvSentMsgHolder);
+        mTvSentMsg = (TextView) findViewById(R.id.tvSentMsg);
     }
 
     public void reply(View view) {
